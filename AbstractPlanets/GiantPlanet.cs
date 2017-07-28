eeb936318ac0b0a4e7923060e7ce6897a3f38bdc
Mr. Hoang Giang Le <hle41@my.centennialcollege.ca>
2017-07-28 15:06:01 -0400

    Working on Planet and GiantPlanet classes

--- AbstractPlanets/GiantPlanet.cs
+++ AbstractPlanets/GiantPlanet.cs
@@ -6,13 +6,60 @@ using System.Text;
 /*
  * Name: Hoang Giang, Le
  * Date: July 28th 2017
- * Description: Assignment 4
- * Version: 0.1 - Create project
+ * Description: This is the Giant Planet class that extends the Planet class
+ * Version: 0.2 - create variables and methods for Giant Planet class
  */
 
 namespace AbstractPlanets
 {
-    public class GiantPlanet
+    /// <summary>
+    /// This is the Giant Planet class
+    /// </summary>
+    public class GiantPlanet : Planet
     {
+        // PRIVATE INSTANCE VARIABLES
+        private string _type;
+
+        // CONSTRUCTOR
+        /// <summary>
+        /// This is the main constructor for the Giant Planet class.
+        /// It takes five parameters - name (string), diameter (double), mass(double), type (string)
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// <param name="mass"></param>
+        /// <param name="type"></param>
+        public GiantPlanet(string name, double diameter, double mass, string type)
+            : base(name, diameter, mass)
+        {
+            this._type = type;
+        }
+        // PUBLIC METHODS
+
+        /// <summary>
+        /// This method that will return true if the MoonCount property is greater than zero
+        /// </summary>
+        /// <returns>bool</returns>
+        public bool HasMoons()
+        {
+            if (this.MoonCount > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// This method that will return true if the RingCount property is greater than zero
+        /// </summary>
+        /// <returns>bool</returns>
+        public bool HasRings()
+        {
+            if (this.RingCount > 0)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
--- AbstractPlanets/Planet.cs
+++ AbstractPlanets/Planet.cs
@@ -6,14 +6,114 @@ using System.Text;
 /*
  * Name: Hoang Giang, Le
  * Date: July 28th 2017
- * Description: Assignment 4
- * Version: 0.1 - Create project
+ * Description: This is the Abstract Planet class
+ * Version: 0.2 - Add some comments
  */
 
 namespace AbstractPlanets
 {
+    /// <summary>
+    /// This is the Abstract Planet class
+    /// </summary>
     public abstract class Planet
     {
- 
+        // PRIVATE INSTANCE VARIABLES
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+
+        // PUBLIC PROPERTIES
+        public double Diameter
+        {
+            get
+            {
+                return this._diameter;
+            }
+        }
+        public double Mass
+        {
+            get
+            {
+                return this._mass;
+            }
+        }
+
+        public int MoonCount
+        {
+            get
+            {
+                return this._moonCount;
+            }
+            set
+            {
+                _moonCount = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name = Name;
+            }
+        }
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this._orbitalPeriod;
+            }
+            set
+            {
+                _orbitalPeriod = value;
+            }
+        }
+        public int RingCount
+        {
+            get
+            {
+                return _ringCount;
+            }
+            set
+            {
+                _ringCount = value;
+            }
+        }
+        public double RotationPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+            set
+            {
+                _rotationPeriod = value;
+            }
+        }
+
+        // CONSTRUCTORS
+        /// <summary>
+        /// This is the main constructor for the Planet class.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// <param name="mass"></param>
+        public Planet(string name, double diameter, double mass)
+        {
+            this._name = name;
+            this._diameter = diameter;
+            this._mass = mass;
+        }
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
+        public override string ToString()
+        {
+            return string.Format("\n The planet {0} has {1} diameter and {2} mass.", this.Name, this.Diameter, this.Mass);
+        }
     }
 }
