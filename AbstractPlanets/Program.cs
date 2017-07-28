1b4ccf45c225646579773c8be341bcbb8ca8fb42
Mr. Hoang Giang Le <hle41@my.centennialcollege.ca>
2017-07-28 13:41:11 -0400

    Create project

---
+++ AbstractPlanets/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Hoang Giang, Le
+ * Date: July 28th 2017
+ * Description: Assignment 4
+ * Version: 0.1 - Create project
+ */
+
+namespace AbstractPlanets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ AbstractPlanets/TerrestrialPlanet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Hoang Giang, Le
+ * Date: July 28th 2017
+ * Description: Assignment 4
+ * Version: 0.1 - Create project
+ */
+
+namespace AbstractPlanets
+{
+    public class TerrestrialPlanet
+    {
+    }
+}
